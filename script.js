6b0ee404f9296facb7da047978df3b7d1ad62e96
jwhittedh <jwhittedharris1@gmail.com>
2023-02-10 11:41:39 -0500

    added variable and arrays to js file

--- script.js
+++ script.js
@@ -1,11 +1,28 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+var passwordCriteria = {
+  alphabeticalCharacters: ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+  numbers:["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
+  specialCharacters:["!", "#", "$", "%", "&"],
+  createPasswordCriteria: function() {
+  console.log(this.alphabeticalCharacters[2] + this.numbers[4] + this.specialCharacters[3]);
+}
+}
+
+
+
+//Write password to the #password input
+
+function generatePassword() {
+  prompt("How many characters would you like your password to conatin?");
+
+
+}
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 
 }
